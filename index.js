21ace42375ff577f38005a449b35cf9f2ea24952
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-08-12 22:33:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //Array kitten returns "Milo", "Otis", and "Garfield"
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
